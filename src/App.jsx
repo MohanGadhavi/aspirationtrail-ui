c47e9f625ac8fb90501c4df09529628da5f5ca99
MohanGadhavi <gadhavimohan71@gmail.com>
2025-01-12 13:25:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <h1 className="text-5xl font-extrabold text-red-400">AspirationTrail</h1>
+    </div>
+  );
+};
+
+export default App;
