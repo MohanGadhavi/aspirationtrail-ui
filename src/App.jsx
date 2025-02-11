01b759d96e7e9d1ea5fdb9fed96d8ecf65b8bcdb
[200~AkshayBhavsar5git config --global user.name AkshayBhavsar5git config --global user.name [200~AkshayBhavsar5 <bhavsarakshay786@gmail.com>
2025-02-11 21:13:11 +0530

    static card and logout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,53 @@
-import React, { useEffect } from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import api from "./utils/api.js";
-import Header from "./components/Header.jsx";
-import Login from "./Pages/Login/Login.jsx";
-import ProtectedRoute from "./Routes/ProtectedRoute.jsx";
-import { loginSuccess, logout } from "./store/auth.js";
-import LandingPage from "./Pages/LandingPage/index.jsx";
-import Home from "./Pages/Home/index.jsx";
+import React, { useEffect } from 'react';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import api from './utils/api.js';
+import Header from './components/Header.jsx';
+import Login from './Pages/Login/Login.jsx';
+import ProtectedRoute from './Routes/ProtectedRoute.jsx';
+import { loginSuccess, logout } from './store/auth.js';
+import LandingPage from './Pages/LandingPage/index.jsx';
+import Home from './Pages/Home/index.jsx';
+import Mentor from './Pages/Mentor/Mentor.jsx';
 
 const App = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  console.log("isAuthenticated::::: ", isAuthenticated);
+  console.log('isAuthenticated::::: ', isAuthenticated);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem("authToken");
+    const token = localStorage.getItem('authToken');
     if (token) {
       try {
         api
-          .get("/user/verify-token")
+          .get('/user/verify-token')
           .then((res) => {
-            console.log("resssssssssssss ", res);
+            console.log('resssssssssssss ', res);
             dispatch(loginSuccess(res.data.user));
-            navigate("/home", { replace: true });
+            navigate('/home', { replace: true });
           })
           .catch(() => {
-            localStorage.removeItem("authToken");
+            localStorage.removeItem('authToken');
             dispatch(logout());
-            navigate("/login", { replace: true });
-            console.log("error while verifying token");
+            navigate('/login', { replace: true });
+            console.log('error while verifying token');
           });
       } catch (error) {
-        console.log("Invalid token:", error);
-        console.error("Invalid token:", error);
-        localStorage.removeItem("authToken");
+        console.log('Invalid token:', error);
+        console.error('Invalid token:', error);
+        localStorage.removeItem('authToken');
       }
     } else {
-      navigate("/welcome", { replace: true });
+      navigate('/welcome', { replace: true });
     }
   }, [dispatch]);
 
   return (
     <div>
       <Routes>
+        
         <Route
           index
           path="/"
@@ -67,6 +69,7 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+        <Route path="/mentor" element={<Mentor />} />
       </Routes>
     </div>
   );
